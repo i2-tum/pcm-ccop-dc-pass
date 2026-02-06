03efc7667278c43281b16b8539370b72a698516f
Innocenzo <innocenzofulginiti@gmail.com>
2026-02-06 21:21:26 +0100

    minor

--- src/main.py
+++ src/main.py
@@ -6,26 +6,55 @@ qr = QuantumRegister(6)
 cr = ClassicalRegister(6)
 qc = QuantumCircuit(qr, cr)
 
+# Case where I noticed that I have to fix the generation of the instance
+# With BigProbabilisticGate
 qc.h(0)
 qc.cx(0, 1)
 
-qc.measure(qr[0], cr[0])
+qc.h(2)
+qc.cx(2, 3)
 
-with qc.if_test((cr[0], 1)) as else_:
-    qc.x(5)
-    qc.h(2)
+qc.measure(0, 0)
+qc.measure(1, 1)
+qc.measure(2, 2)
+qc.measure(3, 3)
 
-with else_:
+with qc.if_test(expr.bit_and(cr[0], expr.bit_and(cr[1], expr.bit_and(cr[2], cr[3])))):
     qc.x(3)
     qc.h(4)
 
-qc.reset(qr[1])
-qc.measure(qr[1], cr[1])
+qc.reset(1)
 
-with qc.if_test((cr[1], 0)) as else_:
-    qc.h(3)
-with else_:
-    qc.x(4)
+
+## Case where I am checking whether the disentangle algorithm is working well
+# qc.h(0)
+# qc.cx(0, 1)
+# qc.h(1)
+# qc.cx(1,2)
+# qc.h(2)
+
+# qc.measure(qr[0], cr[0])
+
+
+# qc.measure(qr[0], cr[0])
+# qc.measure(qr[1], cr[1])
+
+# with qc.if_test((cr, 3)):
+#     qc.x(5)
+#     qc.h(2)
+
+# TODO: do a test on conditions with XOR to test the part:
+# if l.expr == r.expr:
+#   return CondResult(always_false=True)
+
+
+# qc.reset(qr[1])
+# qc.measure(qr[1], cr[1])
+
+# with qc.if_test((cr[1], 0)) as else_:
+#     qc.h(3)
+# with else_:
+#     qc.x(4)
 
 # qc.swap(0, 1)
 # qc.x(2)
@@ -64,11 +93,14 @@ with else_:
 
 print("######################### INITIAL CIRC")
 print(qc.draw())
-table, new_qc = ConstantPropagation.optimize(qc, max_ent_group_size=10)
+table, new_qc, info = ConstantPropagation.optimize(qc, max_ent_group_size=10)
 print(table)
 print("######################### PROB CIRC")
 print(new_qc.draw())
 
 print("######################### ISTANCE")
-istnc = ConstantPropagation.generate_istance(new_qc)
-print(istnc)
+instnc = ConstantPropagation.generate_instance(new_qc)
+print(instnc)
+
+print("######################### INFO")
+print(info)
